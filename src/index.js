1d9622922a80568e39e300f0f97ae3de57e0ceda
Bohdan Tarlupa <btarlupa@gmail.com>
2018-04-16 13:51:26 +0300

    small changes2

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
+import 'react-dates/initialize';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
-import 'react-dates/initialize';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
